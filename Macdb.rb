371af236d20a715b744e702a609bdd4fe9818053
jamesdewar <james.dewar@ividata.com>
2014-06-25 11:00:25 +0200

    text file hashes complted

--- Macdb.rb
+++ Macdb.rb
@@ -5,7 +5,8 @@ require 'pry'
 require 'pp'
 
 
-response = HTTParty.get( 'http://www.macvendorlookup.com/vendormacs-xml-download')
+response = HTTParty.get( 'http://www.macrequire 'nokogiri'
+vendorlookup.com/vendormacs-xml-download')
 doc = Nokogiri::XML(response)
 
 items_mac_prefix = doc.xpath("//xmlns:VendorMapping")
@@ -16,6 +17,7 @@ items_mac_prefix.each do |element|
 	list[element["mac_prefix"]] = element["vendor_name"]  
 end
 pp list
+puts list.length
 #binding.pry
 
 
--- text_regex.rb
+++ text_regex.rb
@@ -1,15 +1,20 @@
 require 'pry'
-require 'nokogiri'
 require 'HTTParty'
+require 'pp'
 
 text = HTTParty.get('https://code.wireshark.org/review/gitweb?p=wireshark.git;a=blob_plain;f=manuf;hb=HEAD')
 #puts response.code
 i=0
 #binding.pry
+list_text_vendor_name = {}
+list_text_vendor_detail = {}
+
 text.each_line do | line|
-	if /^(([0-9-A-F]){2}+[\\:]){2}+([0-9-A-F]{2})/.match(line)
-		puts i
-		i+=1
+	if /^(([0-9-A-F]){2}+[\\:]){2}+([0-9-A-F]{2})+\s/.match(line)
+		list_text_vendor_name [line[/^(([0-9-A-F]){2}+[\\:]){2}+([0-9-A-F]{2})/]] = line[/(?<=\t)\w+/]
+		list_text_vendor_detail[line[/(?<=\t)\w+/]] = line[/(?<=[#]).+/]
 	end
-	
 end
+
+pp list_text_vendor_name
+pp list_text_vendor_detail
